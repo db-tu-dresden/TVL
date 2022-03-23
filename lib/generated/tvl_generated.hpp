abd6dbb23beb9de9ead389290df2dc6d522b3164
GitHub Action (Maintainer: Johannes Pietrzyk) <johannes.pietrzyk@tu-dresden.de>
2022-03-23 13:37:17 +0000

    updated SSE sources, generated with db-tu-dresden/TVLGen@f1b51158aed1de88660a26952adbd847b93d1a63

--- lib/generated/tvl_generated.hpp
+++ lib/generated/tvl_generated.hpp
@@ -18,15 +18,14 @@
 /*
  * @file /home/runner/work/TVLGen/TVLGen/lib/generated/tvl_generated.hpp
  * @date 23.03.2022
- * @brief 
  */
 #ifndef TUD_D2RG_TVL__HOME_RUNNER_WORK_TVLGEN_TVLGEN_LIB_GENERATED_TVL_GENERATED_HPP
 #define TUD_D2RG_TVL__HOME_RUNNER_WORK_TVLGEN_TVLGEN_LIB_GENERATED_TVL_GENERATED_HPP
 
 #include "extensions/simd/intel/sse.hpp"
-#include "definitions/mask/mask_sse.hpp"
-#include "definitions/io/io_sse.hpp"
-#include "definitions/compare/compare_sse.hpp"
 #include "definitions/ls/ls_sse.hpp"
+#include "definitions/mask/mask_sse.hpp"
+#include "definitions/compare/compare_sse.hpp"
+#include "definitions/io/io_sse.hpp"
 
 #endif //TUD_D2RG_TVL__HOME_RUNNER_WORK_TVLGEN_TVLGEN_LIB_GENERATED_TVL_GENERATED_HPP
