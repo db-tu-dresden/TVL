bcff2935e816bcd2d2146cea7a100f8b3a48598a
GitHub Action (Maintainer: Johannes Pietrzyk) <johannes.pietrzyk@tu-dresden.de>
2022-08-22 14:34:44 +0000

    updated Neon sources, generated with db-tu-dresden/TVLGen@33a0eb31c7a217c98ec419fc8513495dcb595d69

--- lib/include/generated/tvl_generated.hpp
+++ lib/include/generated/tvl_generated.hpp
@@ -17,12 +17,12 @@
  *==========================================================================*/
 /*
  * \file /home/runner/work/TVLGen/TVLGen/lib/include/generated/tvl_generated.hpp
- * \date 2022-07-20
+ * \date 2022-08-22
  * \note
  * Git-Local Url : /home/runner/work/TVLGen/TVLGen/generator
  * Git-Remote Url: git@github.com:db-tu-dresden/TVLGen.git
  * Git-Branch    : main
- * Git-Commit    : 6e0d9db (6e0d9db4a214b4db8eedbf84c806d01f3372705d)
+ * Git-Commit    : 33a0eb3 (33a0eb31c7a217c98ec419fc8513495dcb595d69)
  * Submodule(s):
  *   Git-Local Url : primitive_data
  *   Git-Remote Url: git@github.com:db-tu-dresden/TVLPrimitiveData.git
@@ -33,24 +33,24 @@
 #ifndef TUD_D2RG_TVL_HOME_RUNNER_WORK_TVLGEN_TVLGEN_LIB_INCLUDE_GENERATED_TVL_GENERATED_HPP
 #define TUD_D2RG_TVL_HOME_RUNNER_WORK_TVLGEN_TVLGEN_LIB_INCLUDE_GENERATED_TVL_GENERATED_HPP
 
-#include "extensions/simd/arm/neon.hpp"
 #include "extensions/scalar.hpp"
-#include "declarations/binary.hpp"
-#include "declarations/memory.hpp"
-#include "declarations/compare.hpp"
-#include "declarations/io.hpp"
-#include "declarations/ls.hpp"
+#include "extensions/simd/arm/neon.hpp"
 #include "declarations/mask.hpp"
+#include "declarations/io.hpp"
+#include "declarations/compare.hpp"
+#include "declarations/memory.hpp"
+#include "declarations/binary.hpp"
 #include "declarations/calc.hpp"
-#include "definitions/binary/binary_neon.hpp"
-#include "definitions/binary/binary_scalar.hpp"
-#include "definitions/memory/memory_scalar.hpp"
+#include "declarations/ls.hpp"
+#include "definitions/mask/mask_neon.hpp"
+#include "definitions/io/io_neon.hpp"
 #include "definitions/compare/compare_neon.hpp"
 #include "definitions/compare/compare_scalar.hpp"
-#include "definitions/io/io_neon.hpp"
-#include "definitions/ls/ls_neon.hpp"
-#include "definitions/ls/ls_scalar.hpp"
-#include "definitions/mask/mask_neon.hpp"
+#include "definitions/memory/memory_scalar.hpp"
+#include "definitions/binary/binary_neon.hpp"
+#include "definitions/binary/binary_scalar.hpp"
 #include "definitions/calc/calc_neon.hpp"
 #include "definitions/calc/calc_scalar.hpp"
+#include "definitions/ls/ls_neon.hpp"
+#include "definitions/ls/ls_scalar.hpp"
 #endif //TUD_D2RG_TVL_HOME_RUNNER_WORK_TVLGEN_TVLGEN_LIB_INCLUDE_GENERATED_TVL_GENERATED_HPP
